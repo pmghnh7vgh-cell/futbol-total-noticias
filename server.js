b22f39c8ced0ba830b4937454e037f5c08467b24
pmghnh7vgh-cell <pmghnh7vgh@privaterelay.appleid.com>
2026-08-10 00:26:04 -0700

    Actualizar server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/api/partidos-hoy', async (req, res) => {
         const response = await fetch(`https://api-sports.io{hoy}`, {
             method: 'GET',
             headers: {
-                'x-apisports-key': 'TU_CLAVE_DE_API_AQUÍ'
+                'x-apisports-key':762fb45b5838e1bb9ca066aee860f871
             }
         });
         const data = await response.json();

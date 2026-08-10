f255d6ac89a920c93c81f6b40d0e8b6aedad0a8f
pmghnh7vgh-cell <pmghnh7vgh@privaterelay.appleid.com>
2026-08-09 22:20:54 -0700

    Actualizar server.js

--- server.js
+++ server.js
@@ -1,14 +1,4 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios'); 
-
-const app = express();
-const PORT = process.env.PORT || 3000; 
-
-app.use(cors());
-app.use(express.json()); 
-
-app.get('/api/partidos', async (req, res) => {
+pp.get('/api/partidos', async (req, res) => {
 try {
 const apiKey = process.env.FOOTBALL_API_KEY; 
 

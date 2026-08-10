bbc7d15beefff1e6f538391f4fc1b8181114c0e2
pmghnh7vgh-cell <pmghnh7vgh@privaterelay.appleid.com>
2026-08-09 21:31:33 -0700

    Actualizar server.js

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-ORT = process.env.PORT || 3000; 
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios'); 
+
+const app = express();
+const PORT = process.env.PORT || 3000; 
 
 app.use(cors());
 app.use(express.json()); 
